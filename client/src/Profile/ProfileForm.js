3ff4169c4d1db47e2a6123f5db8c76d719fdfdef
Francis Li <mail@francisli.com>
2021-05-06 17:55:08 -0700

    Update ProfileForm to include User registration fields, call auth.register on new/create

--- client/src/Profile/ProfileForm.js
+++ client/src/Profile/ProfileForm.js
@@ -6,8 +6,10 @@ function ProfileForm() {
     const {id} = useParams();
     const history = useHistory();
     const [profile, setProfile] = useState({
-        name: '',
-        position: 0,
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: ''
     });
 
     useEffect(function(){
@@ -28,7 +30,7 @@ function ProfileForm() {
             if (id) {
                 await Api.users.update(id, profile); // when it's singular, it becomes dark blue..not sure if this is right + 31, 45, 49, 53 lines
             } else {
-                await Api.users.create(profile);
+                await Api.auth.register(profile);
             }
             history.push('/profiles');
         } catch (error) {
@@ -40,17 +42,25 @@ function ProfileForm() {
         <main className="container">
         <h1>Profile Form</h1>
         <form onSubmit={onSubmit}>
-        <div className="mb-3">
-            <label className="form-label">Name</label>
-            <input className="form-control" type="text" name="name" value={profile.name} onChange={onChange} />
-        </div>
-        <div className="mb-3">
-            <label className="form-label">Position</label>
-            <input className="form-control" type="text" name="position" value={profile.position} onChange={onChange}/>
-        </div>
-        <button className="btn btn-primary" type="submit">Submit</button>
+            <div className="mb-3">
+                <label className="form-label" htmlFor="firstName">First name</label>
+                <input type="text" class="form-control" id="firstName" name="firstName" onChange={onChange} value={profile.firstName} />
+            </div>
+            <div className="mb-3">
+                <label className="form-label" htmlFor="lastName">Last name</label>
+                <input type="text" class="form-control" id="lastName" name="lastName" onChange={onChange} value={profile.lastName} />
+            </div>
+            <div className="mb-3">
+                <label className="form-label" htmlFor="email">Email</label>
+                <input type="text" class="form-control" id="email" name="email" onChange={onChange} value={profile.email} />
+            </div>
+            <div className="mb-3">
+                <label className="form-label" htmlFor="password">Password</label>
+                <input type="password" class="form-control" id="password" name="password" onChange={onChange} value={profile.password} />
+            </div>
+            <button className="btn btn-primary" type="submit">Submit</button>
         </form>
-        <p>{JSON.stringify(profile.name)}</p>
+        <p>{JSON.stringify(profile)}</p>
     </main>
     )
 }
